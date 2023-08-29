4467dae4e56d84d04bb756258487f737d197638d
李双全 <294388344@qq.com>
2023-08-29 09:13:07 +0800

    升级为.net4.7.2

--- CSInterface/CSWrapper.cs
+++ CSInterface/CSWrapper.cs
@@ -5,12 +5,13 @@ using System.Text;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Diagnostics;
 
 namespace CSInterface
 {
     [ComVisible(true)]
     [Guid("35A5CE1E-551C-41EC-81D4-005318550119")]
-    public class CSWrapper:ICSWrapper
+    public class CSWrapper : ICSWrapper
     {
         public CSWrapper()
         {
@@ -24,7 +25,7 @@ namespace CSInterface
         private Assembly LoadAssembly(string assemblyName)
         {
             Assembly asm = null;
-            if (assemblyName.ToLower().Contains(".dll")||assemblyName.ToLower().Contains(".exe"))
+            if (assemblyName.ToLower().Contains(".dll") || assemblyName.ToLower().Contains(".exe"))
             {
                 //说明是路径
                 if (!Path.IsPathRooted(assemblyName))
@@ -37,15 +38,21 @@ namespace CSInterface
                         assemblyName = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, assemblyName);
                     }
                 }
+                SetWorkDir(Path.GetDirectoryName(assemblyName));
                 asm = Assembly.LoadFrom(assemblyName);
             }
             else
             {
+                SetWorkDir(Path.GetDirectoryName(assemblyName));
                 asm = Assembly.Load(assemblyName);
             }
             return asm;
         }
-        public object InvokeMemberMethod(string assemblyName, string methodName, bool hasParas,object[] paras)
+        private void SetWorkDir(string dir)
+        {
+            Process.GetCurrentProcess().StartInfo.WorkingDirectory = dir;
+        }
+        public object InvokeMemberMethod(string assemblyName, string methodName, bool hasParas, object[] paras)
         {
             Assembly asm = LoadAssembly(assemblyName);
             foreach (var type in asm.GetTypes())
@@ -59,9 +66,9 @@ namespace CSInterface
                     var obj = Activator.CreateInstance(type);
                     if (!hasParas)
                     {
-                        return method.Invoke(obj,null);
+                        return method.Invoke(obj, null);
                     }
-                    else 
+                    else
                     {
                         return method.Invoke(obj, paras);
                     }
@@ -69,23 +76,23 @@ namespace CSInterface
             }
             return null;
         }
-        public object InvokeStaticMethod(string assemblyName, string methodName,bool hasParas, object[] paras)
+        public object InvokeStaticMethod(string assemblyName, string methodName, bool hasParas, object[] paras)
         {
             Assembly asm = LoadAssembly(assemblyName);
-            
+
             foreach (var type in asm.GetTypes())
             {
                 if (!type.IsClass)
                     continue;
                 //获取类下面的方法
-                var method = type.GetMethod(methodName, BindingFlags.InvokeMethod|BindingFlags.Public| BindingFlags.Static);
+                var method = type.GetMethod(methodName, BindingFlags.InvokeMethod | BindingFlags.Public | BindingFlags.Static);
                 if (method != null)
                 {
                     if (!hasParas)
                     {
-                        return method.Invoke(null,null);
+                        return method.Invoke(null, null);
                     }
-                    else 
+                    else
                     {
                         return method.Invoke(null, paras);
                     }
@@ -93,7 +100,7 @@ namespace CSInterface
             }
             return null;
         }
-        public object CreateInstance(string assemblyName, string className,bool hasParas,object[] paras)
+        public object CreateInstance(string assemblyName, string className, bool hasParas, object[] paras)
         {
             Assembly asm = LoadAssembly(assemblyName);
             foreach (var type in asm.GetTypes())
@@ -125,18 +132,22 @@ namespace CSInterface
                 {
                     types[i] = paras[i].GetType();
                 }
-                method = obj.GetType().GetMethod(methodName,types);
+                method = obj.GetType().GetMethod(methodName, types);
                 return method.Invoke(obj, paras);
             }
             else
             {
                 method = obj.GetType().GetMethod(methodName, new Type[] { });
-                return method.Invoke(obj,null);
+                return method.Invoke(obj, null);
             }
         }
         public string GetAppDirectory()
         {
             return Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        }
+        public string TestString(string str)
+        {
+            return str;
         }
     }
 }
